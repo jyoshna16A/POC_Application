f1b7ad13dcd8bb04054852683f528f56fa9f2beb
jyoshna16A <jyoshnamaddula7@gmail.com>
2026-04-07 17:46:54 +0530

    Modid

---
+++ hybrid_dev/hybrid_app/hybrid_app/src/main/java/com/example/controller/TaskController.java
@@ -0,0 +1,37 @@
+package com.example.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Task;
+import com.example.repository.TaskRepository;
+
+@RestController
+@RequestMapping("/api/tasks")
+@CrossOrigin // Allow requests from Ionic app
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+    
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+}
---
+++ hybrid_dev/hybrid_app/hybrid_app/src/main/java/com/example/hybrid_app/HybridAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.hybrid_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HybridAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HybridAppApplication.class, args);
+	}
+
+}
